d664f0780294d0c79f0161be109cf926099655a5
ramunaspl <ramunas.platukis@gmail.com>
2021-03-10 19:55:16 +0200

    initial setup with reacstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.scss';
+import movie from './assets/icons/movie.svg'
+import search from './assets/icons/search.svg'
+import Search from './components/SearchComponent.js'
+// import Test from './components/test.js'
+
+function App() {
+  return (
+    <div>
+      <Search/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/SearchComponent.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import movieIcon from '../assets/icons/movie.svg'
+import searchIcon from '../assets/icons/search.svg'
+import Button from 'react-bootstrap/Button';
+
+class Search extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    render() {
+        return (
+            <Container>
+            </Container>
+        );
+    }
+}
+
+export default Search;
