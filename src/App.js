3056fadd4abec4cf076aec8f7c33af748e4ff8e3
ramunaspl <ramunas.platukis@gmail.com>
2021-03-11 00:02:44 +0200

    ui almost done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
-import movie from './assets/icons/movie.svg'
-import search from './assets/icons/search.svg'
 import Search from './components/SearchComponent.js'
-// import Test from './components/test.js'
+
 
 function App() {
   return (
     <div>
-      <Search/>
+      <Search />
     </div>
-    
   );
 }
 
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,20 +1,38 @@
 import { Component } from 'react';
-import movieIcon from '../assets/icons/movie.svg'
-import searchIcon from '../assets/icons/search.svg'
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import moviesvg from '../assets/icons/movie.svg';
+import searchsvg from '../assets/icons/search.svg';
+
 
 class Search extends Component {
 
     constructor(props) {
         super(props);
 
-        // this.handleSubmit = this.handleSubmit.bind(this);
-
     }
 
     render() {
         return (
-            <Container>
+            <Container className="maincontainer">
+                <Row className="searchbar">
+                    <Col className="d-flex align-items-center justify-content-center">
+
+                        <img className="movie-svg" src={moviesvg} alt="movie-logo" />
+                        <input
+                            autoComplete="off"
+                            type="text"
+                            placeholder="Enter movie name"
+                        // value={query}
+                        // onChange={handleSubmit}
+                        />
+                        <button type="submit">
+                            <img src={searchsvg} alt="movie-logo" />
+                        </button>
+                    </Col>
+                </Row>
             </Container>
         );
     }
